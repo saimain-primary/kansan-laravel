832dca4e43525fdec38524f08cf8d89cf8949cb7
Sai Main <saimain.primary@gmail.com>
2023-11-23 12:27:57 +0630

    init

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/webhook', [WebhookController::class,'getWebhook']);
-Route::post('/webhook', [WebhookController::class,'postWebhook']);
